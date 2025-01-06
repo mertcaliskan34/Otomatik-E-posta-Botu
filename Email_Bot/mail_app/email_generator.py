ce8fd473182c3ff7cd23ee3b235c31e435e24f3e
mertcaliskan <mert_caliskan34@hotmail.com>
2025-01-06 11:10:20 +0300

    Prompt upgraded

--- Email_Bot/mail_app/email_generator.py
+++ Email_Bot/mail_app/email_generator.py
@@ -3,9 +3,10 @@ from langchain_core.prompts import ChatPromptTemplate
 
 def generate_llama(email_text):
     template = """
-    Analyze the content, tone, and intent of the email below.
-    Write a reply in the same language, ensuring it aligns with the context,
+    Analyze the content, tone, and intent of the email below and
+    write an example reply in the same language, ensuring it aligns with the context,
     addresses all necessary points, and uses a tone suitable for the situation.
+    Please don't include your analysis in the reply. I just want the example reply.
     Email: {email}
     """
     model = OllamaLLM(model="llama3.2:1b")
